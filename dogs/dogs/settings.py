c7a9f27306918e3f2a89400cf83d9b46fd726cfd
laupardo <35923398+laupardo@users.noreply.github.com>
2021-02-26 12:53:58 -0500

    changes

--- dogs/dogs/settings.py
+++ dogs/dogs/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-tzydbo=%xg4qk*l%u%=(i2!g=s+$$sm4=5n^usdu%&6po)p4j'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
